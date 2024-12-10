371a35d0d7a5977eb3557756b255e6477357f291
Kauan <raidoka546@gmail.com>
2024-12-10 16:01:29 -0400

    feat: created home page

--- src/app/(store)/(home)/page.tsx
+++ src/app/(store)/(home)/page.tsx
@@ -1,44 +1,57 @@
+import { api } from "@/data/api";
+import { Product } from "@/data/types/product";
 import Image from "next/image";
 import Link from "next/link";
 
-interface ProductProps {
-  id: string;
-  name: string;
-  imageUrl: string;
-  price: string;
+// interface ProductProps {
+//   id: string;
+//   name: string;
+//   imageUrl: string;
+//   price: string;
+// }
+
+// const productsMock: ProductProps[] = [
+//   {
+//     id: "1",
+//     name: "Moletom Never Stop Learning",
+//     imageUrl: "/moletom-never-stop-learning.png",
+//     price: "R$129",
+//   },
+//   {
+//     id: "2",
+//     name: "Moletom AI Side",
+//     imageUrl: "/moletom-ai-side.png",
+//     price: "R$99",
+//   },
+//   {
+//     id: "3",
+//     name: "Camiseta Expand Your Mind",
+//     imageUrl: "/camiseta-dowhile-2022.png",
+//     price: "R$69",
+//   },
+// ]
+
+async function getFeaturedProcuts(): Promise<Product[]> {
+  const response = await api('/products/featured')
+
+  const products = await response.json()
+
+  return products
 }
 
-const productsMock: ProductProps[] = [
-  {
-    id: "1",
-    name: "Moletom Never Stop Learning",
-    imageUrl: "/moletom-never-stop-learning.png",
-    price: "R$129",
-  },
-  {
-    id: "2",
-    name: "Moletom AI Side",
-    imageUrl: "/moletom-ai-side.png",
-    price: "R$99",
-  },
-  {
-    id: "3",
-    name: "Camiseta Expand Your Mind",
-    imageUrl: "/camiseta-dowhile-2022.png",
-    price: "R$69",
-  },
-]
+export default async function Home() {
+  const [highlightedProduct, ...otherProducts] = await getFeaturedProcuts()
 
-export default function Home() {
+  console.log(otherProducts)
   return (
     <div className="grid max-h-[860px] grid-cols-9 grid-rows-6 gap-6">
       <Link
-        href="/"
+        href={highlightedProduct.slug}
         className="group relative col-span-6 row-span-6 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
       >
         <Image
           className="group-hover:scale-105 transition-transform duration-500"
-          src="/moletom-never-stop-learning.png"
+          src={highlightedProduct.image}
           alt=""
           width={920}
           height={920}
@@ -47,58 +60,42 @@ export default function Home() {
 
         <div className="absolute bottom-28 right-28 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-4">
           <span className="text-sm truncate">
-            {productsMock[0].name}
+            {highlightedProduct.title}
           </span>
           <span className="flex h-full place-items-center  rounded-full bg-violet-500 px-4 font-semibold">
-          {productsMock[0].price}
+            R${highlightedProduct.price}
           </span>
         </div>
       </Link>
-        <Link 
-          href="/" 
-          className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-        >
-          <Image 
-            className="group-hover:scale-105 transition-transform duration-500"
-            src="/moletom-ai-side.png"
-            alt=""
-            width={450}
-            height={450}
-            quality={80}
-          />
-
-          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-4">
-            <span className="text-sm truncate4">
-              {productsMock[1].name}
-            </span>
-            <span className="flex h-full place-items-center  rounded-full bg-violet-500 px-4 font-semibold">
-              {productsMock[1].price}
-            </span>
-          </div>
-        </Link>
-        <Link 
-          href="/" 
-          className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
-        >
-          <Image 
-            className="group-hover:scale-105 transition-transform duration-500"
-            src="/camiseta-dowhile-2022.png" 
-            alt="" 
-            width={450}
-            height={450} 
-            quality={80} 
-          />
-
-          <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-4">
-            <span className="text-sm truncate">
-              {productsMock[2].name}
-            </span>
-            <span className="flex h-full place-items-center  rounded-full bg-violet-500 px-4 font-semibold">
-              {productsMock[2].price}
-            </span>
-          </div>
-        </Link>
+        {otherProducts.map(product => {
+          return (
+            <Link
+              key={product.id}
+              href={product.slug}
+              className="group relative col-span-3 row-span-3 rounded-lg bg-zinc-900 overflow-hidden flex justify-center items-end"
+            >
+            <Image 
+              className="group-hover:scale-105 transition-transform duration-500"
+              src={product.image}
+              alt=""
+              width={450}
+              height={450}
+              quality={80}
+            />
+  
+            <div className="absolute bottom-10 right-10 h-12 flex items-center gap-2 max-w-[280px] rounded-full border-2 border-zinc-500 bg-black/60 p-1 pl-4">
+              <span className="text-sm truncate4">
+                {product.title}
+              </span>
+              <span className="flex h-full place-items-center  rounded-full bg-violet-500 px-4 font-semibold">
+                R${product.price}
+              </span>
+            </div>
+          </Link>
+          )
+        })}
       </div>
-  );
+  
+)
 }
   
