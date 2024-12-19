8c5f914480a7a777b210fa9249733d47c2a3ed01
Kauan <raidoka546@gmail.com>
2024-12-19 10:41:46 -0400

    fix: forgot the spread op to props

--- src/components/skeleton/skeleton.tsx
+++ src/components/skeleton/skeleton.tsx
@@ -1,7 +1,7 @@
 import { ComponentProps } from "react";
 import { twMerge } from "tailwind-merge";
 
-export function Skeleton({className, props}: ComponentProps<'div'>) {
+export function Skeleton({className, ...props}: ComponentProps<'div'>) {
     return (
         <div className={twMerge("animate-pulse bg-zinc-700 w-full h-12 rounded-md", className)} 
         {...props} 
